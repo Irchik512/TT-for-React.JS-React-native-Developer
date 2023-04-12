edac6d5b3ce913d458ba21113941291461019870
Irchik512 <chornairchik@gmail.com>
2023-04-12 12:45:34 +0300

    Update AddWordView.jsx

--- src/views/AddWord/AddWordView.jsx
+++ src/views/AddWord/AddWordView.jsx
@@ -1,11 +1,9 @@
-// import {AddForm } from 'components/AddForm/AddForm'
 import Sections  from 'components/Section/Section'
 
 export default function AddWordView() {
   return(
     <Sections> 
       <h2>Add new word</h2>
-      {/* <AddForm /> */}
     </Sections>
   );
 };
